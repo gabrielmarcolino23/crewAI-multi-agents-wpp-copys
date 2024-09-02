87d3553fa887a07f7b235a679fa36f7aafbbe40a
Ian Meirelles <202402630661@alunos.ibmec.edu.br>
2024-09-01 21:53:35 -0300

    teste

--- src/copy_writer/crew.py
+++ src/copy_writer/crew.py
@@ -1,32 +1,36 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from dotenv import load_dotenv
-from crewai_tools import PDFSearchTool
-from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAI
 load_dotenv()
+import yaml
+
+
 
 @CrewBase
 class CopyWriterCrew():
 	"""CopyWriter crew"""
-	agents_config = 'config/agents.yaml'
-	tasks_config = 'config/tasks.yaml'
 
 	def __init__(self):
-		self.openai_llm = ChatOpenAI(model="gpt-4o-mini")
+		with open('config/agents.yaml', 'r', encoding='utf-8') as file:
+			self.agents_config = yaml.safe_load(file)
 
+		with open('config/tasks.yaml', 'r', encoding='utf-8') as file:
+			self.tasks_config = yaml.safe_load(file)
+
+		self.openai_llm = OpenAI(model="gpt-4o")
+		
 	@agent
 	def copywriter(self) -> Agent:
 		return Agent(
 			config=self.agents_config['copywriter'],
 			llm = self.openai_llm,
-			verbose=True
 		)
 
 	@task
 	def copywriter_task(self) -> Task:
 		return Task(
 			config=self.tasks_config['copywriter_task'],
-			tool=PDFSearchTool(),
 			agent=self.copywriter(),
 		)
 
